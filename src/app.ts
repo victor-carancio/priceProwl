e2925e5805e4d229e43fd59528b52bef06be8ee1
victor-carancio <victor.carancio@gmail.com>
2024-07-21 18:12:33 -0400

    feat:

--- src/app.ts
+++ src/app.ts
@@ -11,17 +11,31 @@ import cookieParser from "cookie-parser";
 import swaggerUi from "swagger-ui-express";
 import swaggerSetup from "./docs/swaggeer";
 import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 dotenv.config();
 const app = express();
 
-// app.set("trust proxy",1);
+app.set("trust proxy", 1);
 app.use(cors());
+app.use(helmet());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 100 requests cada 15 min por cliente
+    max: 100,
+    message:
+      "You have exceeded the limit of requests in this time window. Please try again later.",
+  }),
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.json());
 
 app.use("/documentation", swaggerUi.serve, swaggerUi.setup(swaggerSetup));
 
+app.get("/", (_req, res) => {
+  res.redirect("/documentation");
+});
 app.use("/api/v1/game", gamesRoutes);
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/wishlist", wishListRoutes);
