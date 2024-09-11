124b1b3e4e06fa3a7ac2464d9e410f94ae199066
victor-carancio <victor.carancio@gmail.com>
2024-09-11 16:34:52 -0300

    Se eliminan migraciones

--- prisma/migrations/20240906215338_img_store_type_and_currency_added/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "StoreGame" ADD COLUMN     "imgStore" TEXT NOT NULL DEFAULT '-',
-ADD COLUMN     "type" TEXT NOT NULL DEFAULT 'Official';
-
--- AlterTable
-ALTER TABLE "StorePrice" ALTER COLUMN "currency" SET DEFAULT 'CLP';
